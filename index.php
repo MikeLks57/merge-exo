fe79df0ac7c8dcd9933ce600eeac5f01acb4fe54
Mickael Lukasiewicz <mickael_lukasiewicz@hotmail.fr>
2016-11-24 21:32:00 +0100

    Mise en forme CSS de l'index

--- index.php
+++ index.php
@@ -15,7 +15,18 @@
 	<title>Posts</title>
 	<link rel="stylesheet" href="">
 	<style>
-		.nameUser {
+		table, tr, td {
+			border-collapse: collapse;
+		}
+		.comment {
+			background-color: #A0ABFC;
+		}
+		.date {
+			background-color: #C2C2C2;
+			font-weight: lighter;
+		}
+		.name {
+			font-size: 1.2em;
 			font-weight: bold;
 		}
 	</style>
@@ -24,15 +35,25 @@
 
 	<h1>Derniers posts</h1>
 	
-	<?php foreach($posts as $post) : ?>
-		<table>
-			<tr>
-				<td class="nameUser"><?= strip_tags($post['user_name'],'<b><i>') ?></td>
-				<td><?= strip_tags($post['post']) ?></td>
-				<td><?= strip_tags($post['date_post']) ?></td>
-			</tr>
-		</table>
-	<?php endforeach ; ?>
-
+		<?php foreach($posts as $post) : ?>
+			<table>
+				<tr class="name">
+					<td>
+						<?= strip_tags($post['user_name']) ?>
+					</td>
+				</tr>
+				<tr class="comment">	
+					<td>
+						<?= strip_tags($post['post']) ?>
+					</td>
+				</tr>
+				<tr class="date">
+					<td>
+						<em>posté le <?= strip_tags(date('d/m/Y', strtotime($post['date_post']))) ?> à <?= strip_tags(date('h:i:s', strtotime($post['date_post']))) ?></em>
+					</td>
+				</tr>
+			</table>
+		<?php endforeach ; ?>
+	
 </body>
 </html>
